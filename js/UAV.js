f9c8672b0fe0aec31829392db48b9de50e79c153
pathakhimadri <pathakhimadri@gmail.com>
2017-04-11 23:54:16 +0200

    Formation with four children

--- js/UAV.js
+++ js/UAV.js
@@ -34,14 +34,24 @@ class UAV {
   get actualPosition() {
     return p5.Vector.add(this._anchorPosition, this._wobblingOffset);
   }
+  get isClusterHead() {
+    return this._isClusterHead;
+  }
+  set isClusterHead(value) {
+    this._isClusterHead = value;
+  }
+  get uavID() {
+    return this._uavID;
+  }
 
-  constructor(radius, position, color, collisionThreshold, wobblingRadius) {
+constructor(uavID, radius, position, isClusterHead, color, collisionThreshold, wobblingRadius) {
+    this._uavID = uavID;
     this.radius = radius;
     this.anchorPosition = position;
+    this.isClusterHead = isClusterHead;
     this.color = color;
     this._collisionThreshold = collisionThreshold || 50;
     this.wobblingRadius = wobblingRadius;
-
     this._wobblingOffset = createVector(0,0,0);
     this._noiseSeed = {
       "x": random(1000),
@@ -50,6 +60,9 @@ class UAV {
     };
     this._noiseOffset = random(1000);
     this.updateWobblingOffset();
+
+    this._distanceToClusterHead = 0;
+    this._uavDistanceArray = [];
   }
 
   draw() {
@@ -64,6 +77,7 @@ class UAV {
   update(uavArray) {
     if(wobbling) this.updateWobblingOffset();
     if(collision) this.performCollisionAvoidance(uavArray);
+    if(formation) this.performClusterFormation(uavArray);
   }
 
   updateWobblingOffset() {
@@ -96,10 +110,60 @@ class UAV {
     }
   }
 
+  performClusterFormation(uavArray) {
+    let targetReached = true;
+    let numClusterMembers = 4;
+    if(uavArray != null) {
+      if(this.isClusterHead) {
+        this.color = 'yellow';
+        for(var i = 0; i < uavArray.length; i++) {
+          let uav = uavArray[i];
+          if(uav.color != 'red'){
+            uav.color = 'green';
+          }          
+          if(uav._uavID > this._uavID) {
+            if(this._uavDistanceArray.length == uavArray.length - 1) {
+              if(this.distanceTo(uav) - this._uavDistanceArray[uav._uavID - 1] > this.collisionThreshold){
+                this._uavDistanceArray[uav._uavID - 1] = this.distanceTo(uav);
+              }
+            }
+            else {
+              this._uavDistanceArray[uav._uavID - 1] = this.distanceTo(uav);
+            }
+          }
+        }
+        let tempDistanceArray = Array.from(this._uavDistanceArray).sort(function(a, b) {return a - b;});
+        for(var j = 0; j < numClusterMembers; j++) {
+          let distance = tempDistanceArray[j];
+          for(var k = 0; k < this._uavDistanceArray.length; k++) {
+            if(this._uavDistanceArray[k] == distance) {
+              let curUavID = k + 1;
+              uavArray[curUavID].color = 'blue';
+
+              let curPos = uavArray[curUavID].anchorPosition;
+              let clusterHeadPos = this.anchorPosition;
+              let targetPos = createVector(clusterHeadPos.x + this.collisionThreshold,
+                clusterHeadPos.y + Math.pow(-1, ~~(j * 0.5)) * this.collisionThreshold, clusterHeadPos.z + Math.pow(-1, j) * this.collisionThreshold);
+
+                if(this.distanceBetween(curPos, targetPos) > 0.5) {
+                  targetReached = false;
+                  let dir = createVector(targetPos.x - curPos.x, targetPos.y - curPos.y, targetPos.z - curPos.z)
+                  uavArray[curUavID].anchorPosition.add(dir.normalize());
+                }
+              }
+            }
+        }
+        if(targetReached) formation = false;
+      }
+    }
+  }
+
   distanceTo(uav) {
     let pos = this.actualPosition;
     let pos2 = uav.actualPosition;
     return dist(pos.x, pos.y, pos.z, pos2.x, pos2.y, pos2.z);
   }
-
+  distanceBetween(curPos, targetPos) {
+    return dist(curPos.x, curPos.y, curPos.z, targetPos.x, targetPos.y, targetPos.z);
+  }
 }
